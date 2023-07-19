f9d3d101498b3702cb5446fb4ab821867575d41b
zwan06 <rizwanhidayat73@gmail.com>
2023-07-19 22:18:36 +0700

    Layout detail dokter

---
+++ MinPro/MinPro/Controllers/DoctorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinPro.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DetailDoctor()
+        {
+            return View();
+        }
+    }
+}
